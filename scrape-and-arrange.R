593f81db5242152a16534c888aa3605f7b6a580f
SallyFreanOwen <sally.frean@gmail.com>
2021-02-26 11:32:04 +1300

    paging issue solved

--- scrape-and-arrange.R
+++ scrape-and-arrange.R
@@ -75,17 +75,6 @@ library(stringr)
   
   print(paste("Using", search_term, "this scrape finds", result_count, "terms"))
   
-  # # Second page of results (as result_count > 100)
-  # call2 <- paste("https://api.digitalnz.org/v3/records.xml?api_key=M2w8CXHEAaiExaTTxXQG",
-  #                search_term, 
-  #                #usage_term,
-  #                "&per_page=100",
-  #                "&page=2",
-  #               "&sort=date",
-  #                sep = "")
-  # call_page2 <- read_html(call2)
-  
-  # harvest record_id, title, date published, full_text, publisher, links (in two lots if there are two pages)
   harvest <- function(x, y) {output <- x %>% 
     rvest::html_nodes("search") %>% 
     xml2::xml_find_all(paste("//results", y)) %>% 
@@ -112,16 +101,65 @@ library(stringr)
   full_text <- harvest(doc, y)
   #y <- c("")
   # <- harvest(doc, y)
-
+  
   # Arrange scraped results as data frame: 
   call_results_items_df <- data.frame(record_id, title, display_date, full_text, usage, api_link, page_link, publisher)
+  rm(record_id, title, display_date, full_text, usage, api_link, page_link, publisher)
   
   call_results_items_df <- mutate(call_results_items_df, 
-                                  search = search_term_clean)
+                                  search = search_term_clean,
+                                  page = 1)
+  
+ # Second page of results (as result_count > 100)
+ 
+  if(result_count>100 & result_count<200) {
+    page_term <- paste("&per_page=", as.numeric(result_count)-100,sep="")
+    call2 <- paste("https://api.digitalnz.org/v3/records.xml?api_key=M2w8CXHEAaiExaTTxXQG",
+                search_term, 
+                #usage_term,
+                page_term,
+                "&page=2",
+                "&sort=date",
+                sep = "") 
+    call_page2 <- read_html(call2)
+    }
+
+  # harvest record_id, title, date published, full_text, publisher, links (in two lots if there are two pages)
+  if(result_count>100) {doc <- call_page2
+  y <- c("/result/id")
+  record_id <- harvest(doc, y)
+  y <- c("/result/display-date")
+  display_date <- harvest(doc, y)
+  y <- c("/result/source-url")
+  page_link <- harvest(doc, y)
+  y <- c("/result/landing-url")
+  api_link <- harvest(doc, y)
+  y <- c("/result/content-partner")
+  publisher <- harvest(doc, y)
+  y <- c("/result/title")
+  title <- harvest(doc, y)
+  y <- c("/result/usage")
+  usage <- harvest(doc, y)
+  y <- c("/result/fulltext")
+  full_text <- harvest(doc, y)
+  #y <- c("")
+  # <- harvest(doc, y)
+
+  # Arrange scraped results as data frame: 
+  call2_results_items_df <- data.frame(record_id, title, display_date, full_text, usage, api_link, page_link, publisher)
+  rm(record_id, title, display_date, full_text, usage, api_link, page_link, publisher)
+  
+  call2_results_items_df <- mutate(call2_results_items_df, 
+                                  search = search_term_clean,
+                                  page = 2)
+  
+  # Bind pages of search together 
+  results_items_df <- bind_rows(call_results_items_df,call2_results_items_df)
+  
+  }
+  #head(call_results_items_df)
   
 
-  
-  #head(call_results_items_df)
   
 
   # Bind different searches together 
@@ -130,23 +168,37 @@ library(stringr)
   # Arrange scraped results as jsons: 
   call_results_items_json <- serializeJSON(call_results_items_df, pretty = FALSE)
   
+  filename <- paste("data/digital_nz_results_", search_term_clean, ".json", sep = "")
+  
   # Save using search term as filename 
-  save(call_results_items_df, filename = paste("data/digital_nz_results_", search_term_clean, ".json"))
+  write(call_results_items_json, file = filename)
   
   # Encode as json-ld rdf (ON HOLD) 
   
   # Repeat for publishers 
   # call_results_publishers_df <- data.frame(unique(publisher))
- 
-
   
 ########################################  
   
-  #  Additional to-do list: 
+  ###  To-do list: 
+  
   #     - sort out multi-page issue (known as Paging, see "Combining pages of JSON data with jsonlite")
-  #     - add Linked.Art encoding 
-  #     - create sql-query box 
+
   #     - sort out saving multiple search terms into same json database 
   #     - save as json in github 
+  #     - add publishers option 
+  
+  #     - create new GitHub repository to host full public database 
+  #     - add new website to that new rpo 
+  #     - host Quality Assured database in that site, at /data 
+  #     - add text to page with protocols/tikanga 
+  #     - investigate password protection 
+  
+  # LOWER PRIORITY  
+  
+  #     - add Linked.Art encoding 
+  
+  #     - add querying User Interface 
+  #     - create sql-query box
   
   
