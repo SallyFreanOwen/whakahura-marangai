f160fd94e91393345d7933fdb1af0eb2d82fc2c1
SallyFreanOwen <sally.frean@gmail.com>
2021-03-02 15:20:14 +1300

    WIP

--- clean.R
+++ clean.R
@@ -27,10 +27,21 @@ unique(results_raw$usage)
 
 results <- mutate(results, 
               text <- ifelse(usage == "All rights reserved", "See source", full_text))
-results <- rename(results[12], text)
-results <- select(results, full_text)
-rename
+names(results)[13] <- "text"
+results <- dplyr::select(results, -full_text)
 
+names(results)[5] <- "link"
+names(results)[6] <- "api_link"
+
+#
+
+results %>% mutate(Date1 = as.Date(date, format = "%m/%d/%Y"),
+              Date2 = as.Date(paste0("01-",date,"-2013"), format = "%d-%B-%Y"),
+
+              
+short_results <- select(results,
+                        record_id, title, )              
+              
 save(results, file = "data/records_for_quality_assuring.RData")
 
 write.csv(results, file = "data/records_for_quality_assuring.csv")
